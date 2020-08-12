20612ce7af2942f112313bdae5c86850220f66f6
Syed Md. Kamruzzaman <syedmdkamruzzaman@gmail.com>
2020-08-12 15:29:10 +0600

    Create Message and Response Message in RabbitMQ

---
+++ RabbitMQConsumer/RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace RabbitMQConsumer
+{
+    class Program
+    {
+        private const string UserName = "guest";
+        private const string Password = "guest";
+        private const string HostName = "localhost";
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory
+            {
+                HostName = HostName,
+                UserName = UserName,
+                Password = Password
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var channel = connection.CreateModel();
+
+
+            channel.BasicQos(0, 1, false);
+
+            MessageReceiver messageReceiver = new MessageReceiver(channel);
+            channel.BasicConsume("jewelQueue", false, messageReceiver);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RequestRabbitMQ/RequestRabbitMQ/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RequestRabbitMQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserName = "guest";
+            string Password = "guest";
+            string HostName = "localhost";
+
+            var connectionFactory =new RabbitMQ.Client.ConnectionFactory()
+            {
+                UserName = UserName,
+                Password = Password,
+                HostName = HostName
+            };
+
+            var connection = connectionFactory.CreateConnection();
+            var model = connection.CreateModel();
+
+            Console.WriteLine("Creating Exchange...");
+
+            model.ExchangeDeclare("jewelExchange", ExchangeType.Direct);
+
+            model.QueueDeclare("jewelQueue", true, false, false, null);
+            Console.WriteLine("Creating Queue");
+
+            model.QueueBind("jewelQueue", "jewelExchange", "jewelexchange_key");
+            Console.WriteLine("Creating Binding");
+
+            var properties = model.CreateBasicProperties();
+            properties.Persistent = false;
+
+            byte[] messagebuffer = Encoding.Default.GetBytes("Direct Message");
+
+            model.BasicPublish("jewelExchange", "jewelexchange_key", properties, messagebuffer);
+            Console.WriteLine("Message Sent");
+
+            Console.ReadLine();
+
+        }
+    }
+}
